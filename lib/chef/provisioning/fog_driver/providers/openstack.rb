4e605f05e748e4dc8b9745a2161554ddfe29a4dd
bsteinfeld <bradley@steinfeld.ca>
2014-12-05 11:06:04 -0500

    Now allow image_name and flavor_name
    image_name -> image_ref
    flavor_name -> flavor_ref

--- lib/chef/provisioning/fog_driver/providers/openstack.rb
+++ lib/chef/provisioning/fog_driver/providers/openstack.rb
@@ -10,6 +10,28 @@ module FogDriver
       def creator
         compute_options[:openstack_username]
       end
+      
+      def bootstrap_options_for(action_handler, machine_spec, machine_options)
+        bootstrap_options = symbolize_keys(machine_options[:bootstrap_options] || {})
+
+        bootstrap_options[:tags]  = default_tags(machine_spec, bootstrap_options[:tags] || {})
+
+        bootstrap_options[:name] ||= machine_spec.name
+        
+        if !bootstrap_options[:flavor_ref] && bootstrap_options[:flavor_name]
+          bootstrap_options[:flavor_ref] = compute.flavors.flavors.detect { |f| f.name == bootstrap_options[:flavor_name] }.id
+        end
+        
+        if !bootstrap_options[:image_ref] && bootstrap_options[:image_name]
+          bootstrap_options[:image_ref] = image.images.detect { |i| i.name == bootstrap_options[:image_name] }.id
+        end
+        
+        bootstrap_options
+      end
+      
+      def image
+        @image ||= Fog::Image.new(compute_options)
+      end
 
       def self.compute_options_for(provider, id, config)
         new_compute_options = {}
@@ -19,6 +41,7 @@ module FogDriver
           :driver_options => { :compute_options => {} },
           :machine_options => { :bootstrap_options => {} }
         }
+        
         result = Cheffish::MergedConfig.new(new_config, config, new_defaults)
 
         new_compute_options[:openstack_auth_url] = id if (id && id != '')
